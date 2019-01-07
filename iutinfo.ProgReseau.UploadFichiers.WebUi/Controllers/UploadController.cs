7d7a8d530236bc78d41a13edcdcf1ff9371a6959
remy lefevre <remy.lefevre@etud.u-picardie.fr>
2019-01-07 18:55:10 +0100

    ajout du code dans l'index.cshtml ainsi qu'un que dans uploadConstroller.cs. Code fonctionnelle qui permet d'upload un fichier depuis la page web. il suffit de lancer le programme et d'ajouter /upload à la fin de l'adresse web

---
+++ iutinfo.ProgReseau.UploadFichiers.WebUi/Controllers/UploadController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iutinfo.ProgReseau.UploadFichiers.WebUi.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Simple(IFormFile file)
+        {
+            if( file.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            string v_Path = $"Tmp{Path.DirectorySeparatorChar}{file.FileName}";
+
+            using (var flux = new FileStream(v_Path, FileMode.Create, FileAccess.Write)){
+                file.CopyTo(flux);
+            }
+
+                return View(nameof(Index));
+        }
+    }
+}
